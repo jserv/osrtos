f88ade7e5e40d8719a13926f92abe327fdec521d
yplam <yplam@yplam.com>
2026-01-06 09:09:29 +0800

    Update configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://osrtos.com',
   vite: {
     plugins: [tailwindcss()]
   },
-  trailingSlash: 'always'
+
+  trailingSlash: 'always',
+  integrations: [sitemap()]
 });
